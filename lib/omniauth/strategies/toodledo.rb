112982cb6c5387143e4dbb4ecfc28867c47476a8
Tommy Bonderenka <tommy@backstit.ch>
2014-09-11 18:45:13 -0400

    fix

--- lib/omniauth/strategies/toodledo.rb
+++ lib/omniauth/strategies/toodledo.rb
@@ -13,24 +13,24 @@ module OmniAuth
       option :client_options, {
         :site          => 'https://api.toodledo.com',
         :authorize_url => '/3/account/authorize.php',
-        :token_url     => '/3/account/token.php',
+        :token_url     => '/3/account/token.php'
       }
 
-      # uid { raw_info["user"]["id"] }
-      # 
-      # info do
-      #   {
-      #     :email    => raw_info["user"]["email"],
-      #     :name     => raw_info["user"]["display_name"],
-      #     :nickname => raw_info["user"]["username"],
-      #     :image    => raw_info["user"]["avatar"]
-      #   }
-      # end
+      uid { raw_info["user"]["id"] }
+
+      info do
+        {
+          :email    => raw_info["user"]["email"],
+          :name     => raw_info["user"]["display_name"],
+          :nickname => raw_info["user"]["username"],
+          :image    => raw_info["user"]["avatar"]
+        }
+      end
 
       extra { raw_info }
 
       def raw_info
-        @raw_info ||= {}
+        @raw_info ||= access_token.get("http://api.toodledo.com/3/account/get.php").body
       end
 
     end
