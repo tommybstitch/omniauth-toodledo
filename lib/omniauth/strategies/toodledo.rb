25eae9b7448da65734cca39fffa2f91b2caf4b7a
Tommy Bonderenka <tommy@backstit.ch>
2014-09-12 11:07:05 -0400

    debug

--- lib/omniauth/strategies/toodledo.rb
+++ lib/omniauth/strategies/toodledo.rb
@@ -28,15 +28,13 @@ module OmniAuth
       
 
       extra { raw_info }
-      debug_info
       
       def raw_info
         @raw_info ||= access_token.get("http://api.toodledo.com/3/account/get.php").body
-      end
-      
-      def debug_info
-        Rails.logger.info "***************"
-        Rails.logger.info self
+        
+        Rails.logger.info "*************"
+        Rails.logger.info @raw_info
+        Rails.logger.info @raw_info.class
       end
     end
   end
