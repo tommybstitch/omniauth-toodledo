e5c094012435f0960b63b06aef6269bce1682d71
Tommy Bonderenka <tommy@backstit.ch>
2014-09-12 11:28:22 -0400

    fix

--- lib/omniauth/strategies/toodledo.rb
+++ lib/omniauth/strategies/toodledo.rb
@@ -16,7 +16,7 @@ module OmniAuth
         :token_url     => '/3/account/token.php'
       }
 
-      uid { raw_info["userid"] }
+      uid do { raw_info["userid"] } end
       
       info do
         {
